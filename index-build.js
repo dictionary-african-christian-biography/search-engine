45bd7024480abb76e0c151fe6b727729c803a93d
xtrp <36894883+xtrp@users.noreply.github.com>
2021-07-12 13:50:56 -0400

    Split into two builds

---
+++ index-build.js
@@ -0,0 +1,3 @@
+const path = require('path');
+const compileSearchData = require('./src/compile');
+compileSearchData()
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require('path');
-const compileSearchData = require('./src/compile');
-
-compileSearchData();
 
 const defaultConfig = {
   devServer: {
