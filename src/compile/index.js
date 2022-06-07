2975e78ee26d195e1582c75b4600465df3a0b3a5
Dictionary of African Christian Biography <dacb@bu.edu>
2022-06-07 12:51:49 -0400

    Update index.js
    added 'environmentalist', to  tagkeys line 149

--- src/compile/index.js
+++ src/compile/index.js
@@ -153,6 +153,7 @@ const createSiteContentFromPageData = (pageData) => {
     'clergy',
     'diaspora',
     'ecumenist',
+    'environmentalist',
     'evangelist',
     'nationalist',
     'martyr',
